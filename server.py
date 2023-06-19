163e042db4913324da77d13400399fe2fb22dbe0
KennetTech <64315533+KennetTech@users.noreply.github.com>
2023-06-19 11:21:45 +0200

    setting up endpoint to accept contract submission

--- server.py
+++ server.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
+from schemas import ContractModel
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "Hello world"}
+
+@app.post("/contractsubmit")
+def submit_contract(contract_query: ContractModel):
+    """ submit a contract and add to rabbitmq server"""
+
+    name = contract_query.contactor_name
+    country = contract_query.contactor_country
+    location = contract_query.contractor_location
+    accommodation_type = contract_query.contractor_accommodation_type
+    accommodation_unit_size = contract_query.contractor_accommodation_unit_size
+    accommodation_price = contract_query.contractor_accommodation_price
+
+    return contract_query
