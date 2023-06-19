865519040c0d6b47d9861d1071e583068403ed72
KennetTech <64315533+KennetTech@users.noreply.github.com>
2023-06-19 11:10:59 +0200

    adding schema to contract submit

---
+++ schemas.py
@@ -0,0 +1,23 @@
+from pydantic import BaseModel
+
+class ContractModel(BaseModel):
+    contactor_name: str
+    contactor_country: str
+    contractor_location: str
+    contractor_accommodation_type: str
+    contractor_accommodation_unit_size: int
+    contractor_accommodation_price: float
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "contractor_name": "Bella Italia",
+                "contractor_country": "Italia",
+                "contractor_location": "lake garda",
+                "contractor_accommodation_type": "mobilehomes",
+                "contractor_accommodation_unit_size": 4,
+                "contractor_accommodation_price": 6.499
+        }
+    }
+
+
